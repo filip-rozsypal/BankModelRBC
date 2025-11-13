3cc38ade7f7710bdfec07548e09ba01d4b70fc70
Filip Rozsypal <filip.rozsypal@gmail.com>
2025-11-13 15:21:41 +0100

    Update runRBC.m

--- runRBC.m
+++ runRBC.m
@@ -10,10 +10,10 @@ USER = 'Filip';
 % USER = 'Jens';
 
 % model
-% MODEL = 'rbc_v4b2.mod'; % full estiamted model
+MODEL = 'rbc_v4b2.mod'; % full estiamted model
 % MODEL = 'rbc_v5_v0.mod';  % calibrated 1 observable version simpler NE term
 % MODEL = 'rbc_v5_v1.mod';% calibrated 1 observable version RW+drift NE term
-MODEL = 'rbc_v5_v3.mod';% calibrated 1 observable version RW+drift NE term
+
 
 
 
