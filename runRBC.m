d8d8764f15af8074124b69a290ccf00fc9dc478b
Filip Rozsypal <filip.rozsypal@gmail.com>
2025-11-13 22:12:03 +0100

    Update runRBC.m

--- runRBC.m
+++ runRBC.m
@@ -3,22 +3,21 @@
 
 clear all;
 
-% user
+% select user
 USER = 'Filip';
 % USER = 'Rasmus';
 % USER = 'Emil';
 % USER = 'Jens';
 
-% model
-MODEL = 'rbc_v4b2.mod'; % full estiamted model
+% select model
+MODEL = 'rbc_v4b2.mod'; % full estimated model
 % MODEL = 'rbc_v5_v0.mod';  % calibrated 1 observable version simpler NE term
 % MODEL = 'rbc_v5_v1.mod';% calibrated 1 observable version RW+drift NE term
 
 
-
-
 DATA = 'DSGE_DATA_2025_10_30_v3.csv';
 
+%% run the code
 run_dynare;
 
 
